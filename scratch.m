db118175ed75e6229308d978c51c0a76c5c574dd
Shuvrajiit02 <shuvrajiit2002@gmail.com>
2026-05-02 10:57:09 +0000

    Revert "feat: implement motion vector extraction tool and add validation script for deserialized packets"
    This reverts commit 410019e32244a2b24e9d7a5ff2b63413789b3463.

--- scratch.m
+++
@@ -1,7 +0,0 @@
-addpath('matlab');
-addpath('matlab/serialization');
-bits = load('data/extracted_aux_bits.txt');
-disp(size(bits));
-disp(['First 20 bits: ', num2str(bits(1:20)')]);
-p = deserializePinfo(bits);
-disp(['Recovered packets: ', num2str(length(p))]);
